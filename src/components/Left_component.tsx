b441b0278bf390eac3f3db6a3dcdb31f4ed30371
Mohamed Insimam <rminsimam123@gmail.com>
2025-01-28 05:20:59 +0530

    Insi 2nd Commit

--- src/components/Left_component.tsx
+++ src/components/Left_component.tsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React from "react";
 
 const Left_component = () => {
   return (
-    <div>
-      Hi
+    <div className=" overflow-y-auto">
+      <div className="flex flex-col mr-2">
+        <span className="w-full bg-yellow-400 h-32 flex flex-col p-2">
+          <h1 className="font-bold text-2xl text-white">Heading</h1>
+          <p className="pt-1 pb-1 text-gray-100">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate
+            odio ex nulla voluptate quidem, ea est? Ipsum exercitationem, ullam
+            blanditiis accusamus nam nemo placeat facere saepe illo excepturi,
+            natus autem?
+          </p>
+        </span>
+        <span className="w-full bg-yellow-400 h-32 flex flex-col p-2 mt-2">
+          <h1 className="font-bold text-2xl text-white">Heading</h1>
+          <p className="pt-1 pb-1 text-gray-100">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate
+            odio ex nulla voluptate quidem, ea est? Ipsum exercitationem, ullam
+            blanditiis accusamus nam nemo placeat facere saepe illo excepturi,
+            natus autem?
+          </p>
+        </span>
+        <span className="w-full bg-yellow-400 h-32 flex flex-col p-2 mt-2">
+          <h1 className="font-bold text-2xl text-white">Heading</h1>
+          <p className="pt-1 pb-1 text-gray-100">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate
+            odio ex nulla voluptate quidem, ea est? Ipsum exercitationem, ullam
+            blanditiis accusamus nam nemo placeat facere saepe illo excepturi,
+            natus autem?
+          </p>
+        </span>
+        <span className="w-full bg-yellow-400 h-32 flex flex-col p-2 mt-2">
+          <h1 className="font-bold text-2xl text-white">Heading</h1>
+          <p className="pt-1 pb-1 text-gray-100">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate
+            odio ex nulla voluptate quidem, ea est? Ipsum exercitationem, ullam
+            blanditiis accusamus nam nemo placeat facere saepe illo excepturi,
+            natus autem?
+          </p>
+        </span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Left_component
+export default Left_component;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,8 +3,8 @@ import Left_component from "../components/Left_component";
 
 const Home = () => {
   return (
-    <div className="flex flex-col p-2">
-      <div className="flex flex-col items-center pt-2 pr-2 pl-2">
+    <div className="flex flex-col p-2 w-full md:h-screen">
+      <div className="flex flex-col items-center pt-2 pr-2 pl-2 h-1/6">
         <img
           src="https://portal.uovt.ac.lk/front-assets/images/logo/UoVT-icon.png"
           alt="UoVT Logo"
@@ -14,14 +14,21 @@ const Home = () => {
           UoVT Notice Bord
         </h1>
       </div>
-      <div className="flex md:flex-row flex-col pt-5 gap-2">
-        <div className="w-full bg-gray-300 flex lg:flex-col p-2">
+      <div className="flex md:flex-row flex-col pt-5 gap-2 mt-3 h-4/6">
+        <div className="w-full bg-gray-300 flex flex-col p-2">
           <Left_component />
         </div>
-        <div className="w-full bg-gray-300 flex lg:flex-col p-2">
-          <div className="w-full bg-gray-500 flex lg:flex-col mb-2 p-1">Bye 1</div>
+        <div className="w-full bg-gray-300 flex flex-col p-2">
+          <div className="w-full bg-gray-500 flex lg:flex-col mb-2 p-1">
+            Bye 1
+          </div>
           <div className="w-full bg-gray-500 flex lg:flex-col p-1">Bye 2</div>
         </div>
+      </div>
+      <div className="h-1/6 w-full flex items-end">
+        <span className="w-full h-12 flex bg-black text-white justify-center">
+          <p className="self-center">©2025. UoVT. All Rights Reserved.</p>
+        </span>
       </div>
     </div>
   );
